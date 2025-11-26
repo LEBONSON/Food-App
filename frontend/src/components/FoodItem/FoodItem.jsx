a28c8d18d63e0c8b7d6916ed94b79c1b375b3644
LEBONI <lebonilionel@gmail.com>
2025-11-26 13:50:42 +0100

    essaie

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -16,6 +16,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
           <p className="food-item-desc">{description}</p>
           <p className="food-item-price">${price}</p>
           <p>lebonson</p> {/*affiche le nom du restaurant car chaque plat est associé à un restaurant*/}
+	  <p>qctuqriqt test de projet</p>
       </div>
     </div>
   );
