faa1a5a53a93c2629ce48392c5e06c68ca2792c5
LEBONI <lebonilionel@gmail.com>
2025-12-16 21:11:59 +0100

    essaie

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -15,8 +15,8 @@ const FoodItem = ({ id, name, price, description, image }) => {
           </div>
           <p className="food-item-desc">{description}</p>
           <p className="food-item-price">${price}</p>
-          
-      </div>
+          <p>zefzefzefzefz sdfsfsdff</p>
+    </div>
     </div>
   );
 };
