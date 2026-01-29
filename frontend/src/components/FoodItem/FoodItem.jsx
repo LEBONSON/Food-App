df634056711c0efd1e71d5a233ff8e874e94c598
LEBONI <lebonilionel@gmail.com>
2026-01-29 13:13:23 +0100

    feat: affichage de l'icone d'ajout

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -3,16 +3,20 @@ import "./FoodItem.css";
 import { assets } from "../../assets/frontend_assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
-// Gestion du nombre d'items dans le panier 
+// Gestion du nombre d'items dans le panier qui commence à 0 et prend la valeur précédente +1 à chaque clic 
       const [itemCount, setItemCount] = useState(0);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
       <img className="food-item-img" src={image} alt="" />
+      {/* Affichage conditionnel de l'icône d'ajout ou du compteur d'items qui permet d'ajouter ou de retirer des items */}
       {!itemCount 
-           ? <img className="add" onClick={() => setItemCount(prev=>+1)} src={assets.add_icon_white} alt="" />
+           ? <img className="add" onClick={() => setItemCount(prev=>prev+1)} src={assets.add_icon_white} alt="" />
             : <div className="food-item-container">
+                <img onClick={()=> setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                <p>{itemCount}</p>
+                <img onClick={()=> setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
              
               </div> 
 
