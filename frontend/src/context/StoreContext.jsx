2d2d63f527fe925af7b7d2b0f808fb5e3b703c00
LEBONI <lebonilionel@gmail.com>
2026-02-05 18:10:16 +0100

    feat: element du panier

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 
 // création du contexte global pour le store qui sera utilisé dans toute l'application afin d'éviter le prop drilling qui est le passage des props à travers plusieurs niveaux de composants
@@ -6,6 +6,7 @@ export const StoreContext = createContext(null);
 
 /* Composant provider qui va envelopper l'application et fournir le contexte global aux composants enfants */
 const StoreContextProvider = (props) => {
+  const [carditems, setCarditems] = useState
   const contextValue = {
     // ici on peut ajouter des valeurs ou des fonctions globales à partager dans l'application
     food_list,
